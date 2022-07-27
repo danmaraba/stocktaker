47340dab9044c89721f376ededdda6a42aec64dc
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-27 22:17:21 +0300

    changed the background color of the nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,13 @@ import React from "react";
 
 function Navbar() {
   return <nav className="nav">
-    <a href="/" className="site-title">StockTaker</a>
+    <a href="/" className="site-title">STOCKTAKER</a>
     <ul>
       <li><a href="/Home">Home</a></li>
       <li><a href="/About">About</a></li>
-      <li><a href="/Products">Products</a></li>.
+      <li><a href="/Products">Products</a></li>
     </ul>
-
-  </nav>;
+  </nav>
 }
 
 export default Navbar;
