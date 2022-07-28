3743850e0f7c985d211da4de68c119ac9e4fc3cd
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-28 06:58:45 +0300

    added padding-right of 30% to my nav ul

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import About from './About';
 import Home from './Home';
 import Products from './Products';
 import Navbar from './Navbar';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Navbar/>
+    <>
+    <Navbar/>
+     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/products" element={<Products/>}/>
+      </Routes>
+     
     </div>
+
+    </>
+   
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Navbar() {
-  return <nav className="nav">
-    <a href="/" className="site-title">STOCKTAKER</a>
-    <ul>
-      <li><a href="/Home">Home</a></li>
-      <li><a href="/About">About</a></li>
-      <li><a href="/Products">Products</a></li>
-    </ul>
-  </nav>
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        STOCKTAKER
+      </Link>
+      <ul>
+        <li className="active">
+          <Link to="/Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/Products">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
