4a2c26f8af7186557b1a7e94e3a85e76202ada3e
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-29 08:53:48 +0300

    updated the option values in the  category label

--- src/NewInventoryForm.js
+++ src/NewInventoryForm.js
@@ -1,54 +1,79 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-
-function NewInventoryForm({onAddInventory}) {
-  const[formData,setFormData]=useState({
-    name: "", price: "", category: ""
+function NewInventoryForm({ onAddInventory }) {
+  const[name,setName]=useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    category: "",
   });
 
   function handleChange(e) {
-    setFormData({...formData,[e.target.name]: e.target.value})
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch ('https://nameless-retreat-54870.herokuapp.com/inventories',{
-    method: "POST",
-    headers: {
-      "Content-Type" : "application/json"
-    },
-    body: JSON.stringify({name:formData.name, price:formData.price, category:formData.category})
-    
-  })
-   .then(res=>res.json())
-   .then((data)=>{
-    onAddInventory(data) 
-    setFormData({...formData,name:"",price:"",category:""})
-   })
+    fetch("https://nameless-retreat-54870.herokuapp.com/inventories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formData.name,
+        price: formData.price,
+        category: formData.category,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        onAddInventory(data);
+        setFormData({ ...formData, name: "", price: "", category: "" });
+      });
   }
 
   return (
-    
-    <form className='new-inventory-form' onSubmit={handleSubmit}>
+    <form className="new-inventory-form" onSubmit={handleSubmit}>
       <h1>My Stocks</h1>
       <h2>Category Filters</h2>
-      <label className='name'>
-        <input placeholder='Name' name="name" onChange={handleChange} value={formData.name}/>
+      <label className="name">
+        <input
+          placeholder="Name"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
       </label>
       <label>
-        <input  placeholder='Price'name="price" onChange={handleChange} value={formData.price}/>
-      </label>
-      <label className='categoryelement'>
-      <input  placeholder='Category'name="category" onChange={handleChange} value={formData.category}/>
+        <input
+          placeholder="Price"
+          name="price"
+          onChange={handleChange}
+          value={formData.price}
+        />
       </label>
       <label>
-        <input  placeholder='Search'name="search" onChange={handleChange} />
+        Category
+        <select
+          className="category"
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        >
+          <option value="Food">Food</option>
+          <option value="Clothing">Clothing</option>
+          <option value="Utensils">Utensils</option>
+        </select>
       </label>
-      <input className='add-inventory' type="submit" onSubmit={handleSubmit} value="Add Inventory"/>
-
+      <input
+        className="add-inventory"
+        type="submit"
+        onSubmit={handleSubmit}
+        value="Add Inventory"
+      />
     </form>
-   )
- }
+  );
+}
 
-
-export default NewInventoryForm
+export default NewInventoryForm;
