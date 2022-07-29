5d7e04de695816a9e85421eecb36109fcbfc8e51
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-29 06:25:35 +0300

    created the handledeleteonclick function

--- src/Inventories.js
+++ src/Inventories.js
@@ -3,24 +3,24 @@ function Inventories({inventory,onDeleteInventory}) {
   const {id,name,price,category}=inventory;
  console.log(inventory)
 
-//  function handleDeleteonClick() {
-//   fetch(`https://nameless-retreat-54870.herokuapp.com/inventories/${id}`,{
-//     method:"DELETE",
-//   })
-//   .then((response)=>response.json())
-//   .then(()=>{
-//     onDeleteInventory(id)})
+ function handleDeleteonClick() {
+  fetch(`https://nameless-retreat-54870.herokuapp.com/inventories/${id}`,{
+    method:"DELETE",
+  })
+  .then((response)=>response.json())
+  .then(()=>{
+    onDeleteInventory(id)})
   
-// }
-//   return (
-//     <div className='inventories'>
-//       <h3>{name}</h3>
-//       <h3>{price}</h3>
-//       <h3>{category}</h3>
-//       <button className='delete'onClick={handleDeleteonClick}>Delete</button>
+}
+  return (
+    <div className='inventories'>
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+      <h3>{category}</h3>
+      <button className='delete'onClick={handleDeleteonClick}>Delete</button>
         
-//     </div>
-//   )
+    </div>
+  )
 }
 
 export default Inventories
